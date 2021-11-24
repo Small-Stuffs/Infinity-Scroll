559c9ea13f1b18eff0d5360d52651ce8e08746c2
Cedrick Garcia <icedgarcia@gmail.com>
2021-11-24 14:17:15 +0800

    wip

--- script.js
+++ script.js
@@ -5,13 +5,27 @@ let photosArray = [];
 const count = 10;
 const apiKey = `5x-UTQZ6IFEuzm2x2-2JvAjx1ERHilaZD8J9xgZ5OeY`;
 const API_URL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+// Helper for setAttribute on DOM elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
 
 const displayPhotos = () => {
   photosArray.forEach((photo) => {
     const item = document.createElement("a");
-
+    setAttributes(item, { href: photo.links.html, target: "_blank" });
     const img = document.createElement("img");
+
+    // setAttributes(img, {
+    //   src: photo.url.regular,
+    //   alt: photo.alt_description,
+    //   title: photo.add_description,
+    // });
     img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("alt", photo.alt_description);
+    img.setAttribute("title", photo.alt_description);
 
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -30,3 +44,13 @@ const displayPhotos = () => {
     //error
   }
 })();
+
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight - 1000
+  ) {
+    getPhotos();
+    console.log("load more");
+  }
+});
